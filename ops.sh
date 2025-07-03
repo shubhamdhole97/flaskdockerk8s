2446fe3cfe0876ca771e595cd4213fb59702a8eb
shubhamdhole97 <shubhamdhole97@gmail.com>
2025-07-03 14:41:17 +0000

    i

--- ops.sh
+++ ops.sh
@@ -4,4 +4,6 @@ VERSION_NUMBER="${VERSION_NUMBER:-1}"
 cd /home/dhole/flaskdockerk8s
 git pull
 docker build -t custom-img-pyapp:$VERSION_NUMBER .
+docker tag custom-img-pyapp:$VERSION_NUMBER shubhamdhole97/custom-img-pyapp:$VERSION_NUMBER
+docker push shubhamdhole97/custom-img-pyapp:$VERSION_NUMBER
 docker run --rm -p 8000:8000 custom-img-pyapp:$VERSION_NUMBER
