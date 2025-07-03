db9b9b1a8b9a787a6e243de1226ac4e13c68765d
shubhamdhole97 <shubhamdhole97@gmail.com>
2025-07-03 16:07:05 +0000

    ss

--- ops.sh
+++ ops.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 VERSION_NUMBER=$(cat version.txt 2>/dev/null)
 VERSION_NUMBER="${VERSION_NUMBER:-1}"
+
 cd /root/flaskdockerk8s
 git pull
+
 docker build -t custom-img-pyapp:$VERSION_NUMBER .
 docker tag custom-img-pyapp:$VERSION_NUMBER shubhamdhole97/custom-img-pyapp:$VERSION_NUMBER
 docker push shubhamdhole97/custom-img-pyapp:$VERSION_NUMBER
+
+# Update Kubernetes deployment with the new image
+kubectl set image deployment flaskdockerk8s-deployment flaskdockerk8s=shubhamdhole97/custom-img-pyapp:$VERSION_NUMBER
+
 docker run --rm -p 8000:8000 custom-img-pyapp:$VERSION_NUMBER
